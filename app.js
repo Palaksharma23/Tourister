256e4e3ff49007cb4f634be0817b93c6e5df1b51
palaksharma23 <mahimasharma2312@gmail.com>
2023-02-12 19:56:05 +0530

    Added appError utility and updated package.json

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appErrors');
 const tourRouter = require('./Routes/tourRoutes');
 const userRouter = require('./Routes/userRoutes');
 
@@ -50,4 +50,32 @@ app.use('/api/v1/tours', tourRouter);
 
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server}`,
+  });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server}`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err)
+
+  // next(new AppError(`Can't find ${req.originalUrl} on this server}`, 404)); // It will skip all the middlewares and will directly go to Error middleware
+});
+
+// 4 arguments wala is always error handling middleware
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // 50 means internal server error
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
+
+// Note::
+// Middlewares are executed in the order they are written in the code
